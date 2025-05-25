3cbcd6607e755797ed3f7404d12c293e6b3eff24
hirota <aoaoworks766@gmail.com>
2025-05-25 13:02:51 +0900

    fix typo 2

--- work/js/app.js
+++ work/js/app.js
@@ -1,13 +1,29 @@
-document.addEventListener('DOMContentLoaded', () => 
+document.addEventListener('DOMContentLoaded', function() {
     fetch('data/product.json')
     .then(response => response.json())
-    .then(data =>
-        data.forEach(product =>
-            document.getElementById('productTable')
-            .appendChild(Object.assign(document.createElement('tr'), {
-                innerHTML: `<td>${product.id}</td><td>${product.name}</td><td>${product.price}</td><td>${product.description}</td>`
-            }))
-        )
-    )
-    .catch(error => console.error('エラー:', error))
-);
+    .then(datas => {
+        const productTable = document.getElementById('productTable');
+        for (let i = 0; i < datas.length; i++) {
+            const data = datas[i];
+            const table = document.createElement('tr');//表の行
+            productTable.appendChild(table); // 表の中に４個の「tr」（行）ができる
+
+            const tableid = document.createElement('td');//表のIDID列データ(情報）
+            tableid.textContent = data.id ;
+            productTable.appendChild( tableid );
+
+            const tablename = document.createElement('td');//表の商品名列データ(情報）
+            tablename.textContent = data.name ;
+            productTable.appendChild( tablename );
+
+            const tableprice = document.createElement('td');//表の価格列データ(情報）
+            tableprice.textContent = data.price ;
+            productTable.appendChild( tableprice );
+
+            const tabledescription = document.createElement('td');//表のid説明列データ(情報）
+            tabledescription.textContent = data.description ;
+            productTable.appendChild( tabledescription );
+        }
+    })
+    .catch(error => console.error('エラー:', error));
+});
